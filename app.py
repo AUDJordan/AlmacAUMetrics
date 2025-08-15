151d341cd4fb5de07472e7facd435e08aa69c52e
AUDJordan <Daniel.Jordan@almacgroup.com>
2025-08-15 12:26:30 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 import psycopg2
 import os
 
@@ -115,6 +115,11 @@ def metrics_dashboard():
 @app.route('/changeover_dashboard')
 def changeover_dashboard():
     return render_template('changeover_dashboard.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'), 'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
